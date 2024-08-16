1c3e98e975a619653fbefe5374110c58613161a0
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2024-08-16 11:38:12 +0300

    Update

--- src/Components/Blocks/Header/Header.jsx
+++ src/Components/Blocks/Header/Header.jsx
@@ -10,9 +10,9 @@ function Header({ children, active, ...props }) {
                     <img src="/logo.png" alt="" />
                 </Link>
                 <div className={classes.header_items}>
-                    {/* <Link to={'/ip'} className={`${classes.header_items_link} ${active == 'ИП' && classes.header_items_link_active}`}>ИП</Link>
+                    <Link to={'/ip'} className={`${classes.header_items_link} ${active == 'ИП' && classes.header_items_link_active}`}>ИП</Link>
                     <Link to={'/contractors'} className={`${classes.header_items_link} ${active == 'Контрагенты' && classes.header_items_link_active}`}>Контрагенты</Link>
-                    <Link to={'/receivedServices'} className={`${classes.header_items_link} ${active == 'Получатели услуг' && classes.header_items_link_active}`}>Получатели услуг</Link> */}
+                    <Link to={'/receivedServices'} className={`${classes.header_items_link} ${active == 'Получатели услуг' && classes.header_items_link_active}`}>Получатели услуг</Link>
                     <Link to={'/documents'} className={`${classes.header_items_link} ${active == 'Документы' && classes.header_items_link_active}`}>Все документы</Link>
                     <Link to={'/addDocument'} className={`${classes.header_items_link} ${active == 'Создать документ' && classes.header_items_link_active}`}>Создать документ</Link>
                 </div>
